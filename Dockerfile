140d65a12ed323eb489ad6f44ed80e09aa622769
yusuf88811 <tolaganovyusuf00@gmail.com>
2022-07-01 15:49:35 +0500

    songi_20

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,10 @@ WORKDIR /code
 COPY requirements.txt ./code
 COPY . .
 
+RUN apt-get update \
+    && apt-get install -yyq netcat
+
+RUN pip install --upgrade pip    
 RUN pip install -r requirements.txt 
 
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,18 @@
 #!/bin/bosh
 
-# python3 manage.py makemigrations
-# python3 manage.py migrate
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
+if [ "$POSTGRES_DB" = "new_blog-02" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+
+exec "$@"
