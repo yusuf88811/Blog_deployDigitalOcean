f7dd832362265ec068306acfdcf16d5870670d4c
yusuf88811 <tolaganovyusuf00@gmail.com>
2022-07-02 12:59:22 +0500

    songi_53

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+# ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
+
 
 # Application definition
 
