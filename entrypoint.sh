ea0b95e07de3bb1d4cceeea8e9fdb4d477213163
yusuf88811 <tolaganovyusuf00@gmail.com>
2022-07-02 09:46:09 +0500

    songi_30

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/bosh
 
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+if [ "$POSTGRES_DB" = "new_blog-02" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+
+exec "$@"
